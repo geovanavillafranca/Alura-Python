43b3cadbf992ce13fcc3327c944b07fbbd4e272c
Geovana Silva Villafranca <A0137846@redecorp.br>
2022-01-13 09:08:07 -0300

    adicionando pontuacao no jogo de adivinhacao

--- lidando_com_entrada_do_usuario_02/jogos/adivinhacao.py
+++ lidando_com_entrada_do_usuario_02/jogos/adivinhacao.py
@@ -4,8 +4,22 @@ print('*********************************')
 print('Bem-vindo ao jogo de adivinhação!')
 print('*********************************')
 
-tentativas = 3
+tentativas = 0
 numero_secreto = randint(1, 100)
+
+print('Qual o nícel de dificuldade?')
+print('(1) Fácil (2) Médio (3) Difícil')
+
+nivel = int(input('Digite o nível: '))
+
+pontos = 1000
+
+if nivel == 1:
+    tentativas = 15
+elif nivel == 2:
+    tentativas = 10
+elif nivel == 3:
+    tentativas = 5
 
 for rodada in range(1, tentativas + 1):
     print(f'Tentativa {rodada} de {tentativas}')
@@ -24,7 +38,7 @@ for rodada in range(1, tentativas + 1):
 
     else:
         if (acertou):
-            print('Você acertou!!')
+            print(f'Você acertou e fez {pontos} pontos!')
             break
 
         else:
@@ -34,6 +48,9 @@ for rodada in range(1, tentativas + 1):
             
             else:
                 print('Seu chute foi muito baixo')
+
+            pontos_perdidos = abs(numero_secreto - chute) # ex: num_sec= 30 e chute= 10 | 30 - 10 = 20 pont_perdidos
+            pontos -= pontos_perdidos
 
 
 print('Fim de jogo')
