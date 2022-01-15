d2f58fb4730309c9c4436d66470220a1177cc067
Geovana Silva Villafranca <A0137846@redecorp.br>
2022-01-15 14:30:38 -0300

    melhorando minha forca

--- lidando_com_entrada_do_usuario_02/jogos/forca.py
+++ lidando_com_entrada_do_usuario_02/jogos/forca.py
@@ -5,13 +5,33 @@ def jogar():
     print('***Bem-vindo ao jogo de Forca!***')
     print('*********************************')
 
+    palavra_secreta = "python"
+    letras_acertadas = []
+    for v in range(len(palavra_secreta)):
+        letras_acertadas.append('_') 
+
+    print(letras_acertadas)
+
+    enforcou = False
+    acertou = False
+    contador = 0
+    errou = len(palavra_secreta)
+    letras_erradas = []
+    # enquanto nao enforcou e nao acertou
+    while not enforcou and not acertou:
+
+        chute = input('Qual letra? ').strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                letras_acertadas[index] = letra
 
 
+        
 
-
-    print('Fim do jogo')
+            index += 1
+        print(letras_acertadas)
 
 if __name__ == '__main__':
     jogar()
-
-
---
+++ minha_forca.py
@@ -0,0 +1,52 @@
+
+def jogar():
+
+    print('*********************************')
+    print('***Bem-vindo ao jogo de Forca!***')
+    print('*********************************')
+
+    palavra_secreta = "python"
+    letras_acertadas = []
+    enforcou = False
+    acertou = False
+    contador = 0
+    letras_erradas = []
+
+    for v in range(len(palavra_secreta)):
+        letras_acertadas.append('_') 
+
+    print(letras_acertadas)
+
+    errou = len(palavra_secreta)
+    # enquanto nao enforcou e nao acertou
+    while not enforcou and not acertou:
+
+        chute = input('Qual letra? ').strip()
+
+        index = 0
+        if chute not in palavra_secreta:
+            letras_erradas.append(chute)
+            errou -= 1
+            if errou == 0:
+                print('Você se enforcou!')
+                enforcou = True
+            print(f"Seus chutes: {letras_erradas}")
+            
+        else:
+            for letra in palavra_secreta:
+
+                if chute.upper() == letra.upper():
+                    letras_acertadas[index] = letra
+                    contador += 1
+                    if contador == len(palavra_secreta):
+                        print('Parabéns! Você acertou a palavra secreta!')
+                        acertou = True
+
+                index += 1
+
+            print(letras_acertadas)
+
+    print('Fim do jogo')
+
+if __name__ == '__main__':
+    jogar()
