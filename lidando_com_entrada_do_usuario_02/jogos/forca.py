4e02b95891ccb3d81938199f5eb4ac392f3f42b2
Geovana Villafranca <geovanavillafranca@hotmail.com>
2022-01-16 13:25:13 -0300

    organizando o codigo com funcoes

--- lidando_com_entrada_do_usuario_02/jogos/forca.py
+++ lidando_com_entrada_do_usuario_02/jogos/forca.py
@@ -1,27 +1,14 @@
 import random
 
 
+
 def jogar():
 
-    print('*********************************')
-    print('***Bem-vindo ao jogo de Forca!***')
-    print('*********************************')
+    imprime_mensagem_abertura()
 
+    palavra_secreta = carrega_palavra_secreta()
 
-    arquivo = open('palavras.txt', 'r')
-    palavras = []
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
-    arquivo.close()
-
-    numero = random.randrange(0, len(palavras))
-
-    palavra_secreta = palavras[numero].upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
-
-    print(letras_acertadas)
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -52,5 +39,33 @@ def jogar():
 
 
     print('Fim do jogo')
+
+
+def imprime_mensagem_abertura():
+    
+    print('*********************************')
+    print('***Bem-vindo ao jogo de Forca!***')
+    print('*********************************')
+
+
+def carrega_palavra_secreta():
+    arquivo = open('palavras.txt', 'r')
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+    
+
 if __name__ == '__main__':
     jogar()
