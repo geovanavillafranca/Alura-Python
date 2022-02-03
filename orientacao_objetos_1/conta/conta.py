bb0fd739a3620b8ce813fc7ea1a95aee82b8421c
Geovana  Villafranca <geovanavillafranca@hotmail.com>
2022-02-03 16:24:55 -0300

    curso oo basico finalizado

--- orientacao_objetos_1/conta/conta.py
+++ orientacao_objetos_1/conta/conta.py
@@ -6,41 +6,88 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        
+
+    # Get
+    @property   	
+    def numero(self):
+        return self.__numero
+
+    @property   	
+    def titular(self):
+        return self.__titular
+
+    @property   	
+    def saldo(self):
+        return self.__saldo
+
+    @property   	
+    def limite(self):
+        return self.__limite
+
+
+    # Set
+    @numero.setter   	
+    def numero(self, numero):
+        self.__limite = numero
+
+    @titular.setter   	
+    def titular(self, titular):
+        self.__limite = titular
+
+    @limite.setter   	
+    def limite(self, limite):
+        self.__limite = limite
+
     
 
+    # metodo estatico
+    @staticmethod   	
+    def codigos_bancos():
+        return {"BR":"001", "Caixa": "104", "Bradesco": "237"}
+    
+    
+    # metodos 
     def extrato(self):
         print(f'Saldo {self.__saldo} do titular {self.__titular}')
     
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print(f"O valor {valor} passou do limite")
 
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
 
-    @property   	
-    def saldo(self):
-        return self.__saldo
+codigos = Conta.codigos_bancos()
+print(codigos['BR'])
+print(codigos['Caixa'])
+print(codigos['Bradesco'])
 
-
-
-
-
-
+'''
+print(Conta.codigo_banco())
 
 
 conta = Conta(123, 'Nico', 55.0, 1000.0)
+conta.saca(2000.0)
+conta.saldo
 conta2 = Conta(321, 'Joe', 115.0, 1000.0)
 conta.extrato()
 conta2.extrato()
 conta.transfere(15, conta2)
 conta.extrato()
 conta2.extrato()
-'''
+
 print(conta.numero, conta.titular)
 conta.extrato()
 conta.saque(10)
