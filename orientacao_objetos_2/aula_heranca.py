64701d6536ac3c7a2c3085c060a6a0688918aca4
Geovana  Villafranca <geovanavillafranca@hotmail.com>
2022-03-22 20:30:28 -0300

    duck typing

--- orientacao_objetos_2/aula_heranca.py
+++ orientacao_objetos_2/aula_heranca.py
@@ -44,19 +44,53 @@ class Serie(Programa):
     def __str__(self):
         return f"{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes"
 
+
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    # torna iteravel 
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+ 
+
 vingadores = Filme('vingadores | Guerra Infinita', 2018, 160)
-vingadores.dar_likes()
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+
+
+vingadores.dar_likes()
+demolidor.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+
+
 atlanta.dar_likes()
 atlanta.dar_likes()
 
 
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+print(playlist_fim_de_semana[0])
 
-for programa in filmes_e_series:
+for programa in playlist_fim_de_semana:
     print(programa)
 
 
+
+
+print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana}')
 
