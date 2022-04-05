349f40b3877383e3f4c5cd4a12aa18362cf5381a
Geovana  Villafranca <geovanavillafranca@hotmail.com>
2022-04-05 20:21:01 -0300

    utilizando fatiamento com indice

--- string_em_python/extrator_url/main.py
+++ string_em_python/extrator_url/main.py
@@ -26,3 +26,24 @@ indice_valor = indice_parametro + len(parametro_busca) + 1
 valor = url_parametros[indice_valor:]
 
 print('Valor: ', valor)
+
+
+print('#### Arrumando o bug do moedaOrigem ####')
+
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&')
+valor = url_parametros[indice_valor:indice_e_comercial]
+
+print('Valor: ', valor)
+
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+
+print('Valor: ', valor)
+
+
+
