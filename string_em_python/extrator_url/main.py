cf696205cfc3aa04cbf2ab9f930edab812201d46
Geovana  Villafranca <geovanavillafranca@hotmail.com>
2022-04-05 15:57:20 -0300

    usando find e len

--- string_em_python/extrator_url/main.py
+++ string_em_python/extrator_url/main.py
@@ -1,8 +1,28 @@
 print('Fatiando uma String')
-url = "bytebank.com/cambio?moedaOrigem=real"
+url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar"
+
+indice_interrogacao = url.find('?')
+
 print(url)
-url_base = url[0:19]
+print('Aqui vai até a ?')
+url_base = url[:indice_interrogacao]
 print(url_base)
 
-url_parametros = url[20:36]
+print('Aqui vai até o final')
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+
+print('Valor: ', valor)
+
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+
+print('Valor: ', valor)
