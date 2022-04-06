0de1b0ad92ac67d08c6fa11db3cc1f2b4baddbfe
Geovana  Villafranca <geovanavillafranca@hotmail.com>
2022-04-06 16:49:17 -0300

    validacao dos parametros e fatiamento concluido

--- string_em_python/extrator_url/main.py
+++ string_em_python/extrator_url/main.py
@@ -1,49 +1,19 @@
-print('Fatiando uma String')
-url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar"
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
+# Separa base e parâmetros
 indice_interrogacao = url.find('?')
-
-print(url)
-print('Aqui vai até a ?')
 url_base = url[:indice_interrogacao]
-print(url_base)
-
-print('Aqui vai até o final')
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
-
-parametro_busca = 'moedaOrigem'
+# Busca o valor de um parâmetro
+parametro_busca = 'quantidade'
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_parametros[indice_valor:]
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
 
-print('Valor: ', valor)
-
-parametro_busca = 'moedaDestino'
-indice_parametro = url_parametros.find(parametro_busca)
-indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_parametros[indice_valor:]
-
-print('Valor: ', valor)
-
-
-print('#### Arrumando o bug do moedaOrigem ####')
-
-parametro_busca = 'moedaOrigem'
-indice_parametro = url_parametros.find(parametro_busca)
-indice_valor = indice_parametro + len(parametro_busca) + 1
-indice_e_comercial = url_parametros.find('&')
-valor = url_parametros[indice_valor:indice_e_comercial]
-
-print('Valor: ', valor)
-
-parametro_busca = 'moedaDestino'
-indice_parametro = url_parametros.find(parametro_busca)
-indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_parametros[indice_valor:]
-
-print('Valor: ', valor)
-
-
-
+print(valor)
