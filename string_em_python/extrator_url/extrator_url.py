57c9cf9aeebf4814068404b0b1e13a176e746e95
Geovana  Villafranca <geovanavillafranca@hotmail.com>
2022-04-08 14:17:35 -0300

    atualizando a classe ExtratorURL

--- string_em_python/extrator_url/extrator_url.py
+++ string_em_python/extrator_url/extrator_url.py
@@ -1,3 +1,5 @@
+import re
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -13,6 +15,14 @@ class ExtratorURL:
     def valida_url(self):
         if not self.url:
             raise ValueError("A URL está vazia")
+        
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(url)
+
+        if not match:
+            raise ValueError("A URL não é válida.")
+
+        print('A URL é válida.')
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
@@ -36,11 +46,9 @@ class ExtratorURL:
         return valor
 
 
-
-extrator_url = ExtratorURL(None)
-
+url = 'httpq://www.bytebank.com/cambio'
+extrator_url = ExtratorURL(url)
 # extrator_url = ExtratorURL(" ")
-
-
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+
 print(valor_quantidade)
